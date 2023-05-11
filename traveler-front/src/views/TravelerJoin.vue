3b1a0375bff0d8320e3ed62961dffa449577d1af
H4R1B0 <qkrguswns25@naver.com>
2023-05-11 16:55:54 +0900

    feat: 로그인과 회원가입 추가

---
+++ traveler-front/src/views/TravelerJoin.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="join-content">
+    <h1>회원가입</h1>
+    <form v-on:submit.prevent="join">
+      아이디
+      <br />
+      <input type="text" name="member_id" id="member_id" autofocus placeholder="아이디" v-model.trim="member.member_id" />
+      <br />
+      이름
+      <br />
+      <input type="text" name="member_name" id="member_name" autofocus placeholder="이름" v-model.trim="member.member_name" />
+      <br />
+      이메일
+      <br />
+      <input type="email" name="email" id="email" autofocus placeholder="이메일" v-model.trim="member.email" />
+      <br />
+      비밀번호
+      <br />
+      <input type="password" name="member_password" id="member_password" autofocus placeholder="비밀번호" v-model.trim="member.member_password" />
+      <br />
+      비밀번호 확인
+      <br />
+      <input type="password" name="member_password_confirm" id="member_password_confirm" autofocus placeholder="비밀번호 확인" />
+      <br />
+      <input type="submit" value="회원가입" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      member: {
+        member_id: "",
+        member_name: "",
+        member_password: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    join() {
+      console.log("회원가입");
+      // const url = "http://localhost/member/login";
+    },
+  },
+};
+</script>
+
+<style></style>
--- traveler-front/src/views/TravelerLogin.vue
+++ traveler-front/src/views/TravelerLogin.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>로그인 화면</div>
+  <div class="login-content">
+    <h1>로그인</h1>
+    <form v-on:submit.prevent="login">
+      아이디
+      <br />
+      <input type="text" name="member_id" id="member_id" autofocus placeholder="아이디" v-model.trim="member.member_id" />
+      <br />
+      비밀번호
+      <br />
+      <input type="password" name="member_password" id="member_password" autofocus placeholder="비밀번호" v-model.trim="member.member_password" />
+      <br />
+      <input type="checkbox" v-model="member.save_id" />아이디 기억하기
+      <br />
+      <span>아직 회원이 아니신가요? </span>
+      <router-link :to="{ name: 'join' }">회원가입 </router-link>
+      <br />
+      <input type="submit" value="로그인" />
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      member: {
+        member_id: "",
+        member_password: "",
+        save_id: false,
+      },
+    };
+  },
+  methods: {
+    login() {
+      console.log("로그인");
+      // const url = "http://localhost/member/login";
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
