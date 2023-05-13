bf85508a72731fdecb1ac107e1f471f00e4a898a
H4R1B0 <qkrguswns25@naver.com>
2023-05-13 19:44:26 +0900

    feat: 검색 기능 추가

--- traveler-front/src/store/index.js
+++ traveler-front/src/store/index.js
@@ -1,17 +1,57 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    keyword: "_",
+    content: "_",
+    code: 0,
   },
   getters: {
+    keyword: (state) => {
+      return state.keyword;
+    },
+    content: (state) => {
+      return state.content;
+    },
+    code: (state) => {
+      return state.code;
+    },
+    searchData: (state) => {
+      let data = {
+        keyword: state.keyword,
+        content: state.content,
+        code: state.code,
+      };
+      return data;
+    },
   },
   mutations: {
+    setKeyword: (state, keyword) => {
+      state.keyword = keyword;
+    },
+    setContent: (state, content) => {
+      state.content = content;
+    },
+    setCode: (state, code) => {
+      state.code = code;
+    },
   },
   actions: {
+    setKeyword: ({ commit, state }, keyword) => {
+      console.log(state);
+      commit("setKeyword", keyword);
+    },
+    setContent: ({ commit, state }, content) => {
+      console.log(state);
+      commit("setContent", content);
+    },
+    setCode: ({ commit, state }, code) => {
+      console.log(state);
+      commit("setCode", code);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- traveler-front/src/views/tour/TourItem.vue
+++ traveler-front/src/views/tour/TourItem.vue
@@ -1,19 +1,39 @@
 <template>
-  <div>items</div>
+  <li class="tour-list-item">
+    <img class="item-image" :src="attraction.img" alt="관광지" />
+    <ul>
+      <li class="item-title">{{ attraction.title }}</li>
+      <li class="item-addr">{{ attraction.addr }}</li>
+    </ul>
+  </li>
 </template>
 
 <script>
 export default {
-  name: "",
-  components: {},
-  data() {
-    return {
-      message: "",
-    };
+  props: {
+    attraction: Object,
   },
-  created() {},
-  methods: {},
 };
 </script>
 
-<style scoped></style>
+<style>
+.tour-list-item {
+  display: flex;
+  flex-basis: 30%;
+  margin-bottom: 2em;
+  margin-left: 10%;
+}
+.item-image {
+  width: 10em;
+  border-radius: 10px;
+  margin-top: 0.5em;
+  margin-right: 1em;
+}
+.item-title {
+  font-weight: bold;
+  width: 120px;
+}
+.item-addr {
+  width: 100px;
+}
+</style>
